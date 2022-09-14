9aa206a8c401bf5be36d3268a1da623958056b63
jaino <msb3302@naver.com>
2022-09-14 22:42:11 +0900

    Chore : rename file, di ledStateRepository

--- app/src/main/java/com/example/wherebeam/di/RemoteModule.kt
+++ app/src/main/java/com/example/wherebeam/di/RemoteModule.kt
@@ -1,20 +1,31 @@
 package com.example.wherebeam.di
 
+import com.example.wherebeam.data.LedStateRepositoryImpl
 import com.example.wherebeam.data.SensorStateRepositoryImpl
+import com.example.wherebeam.domain.LedStateRepository
 import com.example.wherebeam.domain.SensorStateRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import javax.inject.Singleton
 
+@ExperimentalCoroutinesApi
 @Module
 @InstallIn(SingletonComponent::class)
-object SensorStateModule {
+object RemoteModule {
     @Provides
     @Singleton
     fun provideSensorStateRepository(
     ): SensorStateRepository {
         return SensorStateRepositoryImpl()
+    }
+
+    @Provides
+    @Singleton
+    fun provideLedStateRepository(
+    ): LedStateRepository{
+        return LedStateRepositoryImpl()
     }
 }
